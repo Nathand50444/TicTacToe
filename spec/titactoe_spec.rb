d0d087c7dbab782a59271beae95734f263e3cd51
Nathan <139653121+Nathand50444@users.noreply.github.com>
2024-10-23 21:16:40 +0100

    Comments created for basis of tests in spec.

--- spec/titactoe_spec.rb
+++ spec/titactoe_spec.rb
@@ -1,2 +1,11 @@
 require '../spec/tictactoe'
 
+# Checkk all outcomes for game_over to be triggered
+
+# 1. When there are no " " entries left then 'game_over' is true
+
+# 2. When "X X X" combination is present then 'game_over' is true
+
+# Current player returns 'X' when the turn_count is even and 'O' when odd
+
+# Check outcomes for position taken when provided with data.
