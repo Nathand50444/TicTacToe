79b264d077e5c194c87b1693aed041a7e0e8a9bc
NathanD <nathand50444@gmail.com>
2024-10-25 22:09:53 +0100

    valid_move? test created
    Testing confirms that inputs for position 0 are 'valid' when empty and 'not valid' when occupied.

--- lib/tictactoe.rb
+++ lib/tictactoe.rb
@@ -2,7 +2,6 @@ class TicTacToe
 
     def initialize
         @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]   # Initialize sets the @board variable to a new, empty array that represents the game board.
-        turn
     end
 
     WIN_COMBINATIONS = [
@@ -45,7 +44,8 @@ class TicTacToe
 
     def turn
         puts "Choose a position (1-9): "          # The 'turn' method manages each player's turn in the game.
-        position = gets.strip                     
+        position = gets.strip
+        puts position
         index = input_to_index(position)          # The player inputs their chosen position which is converted to an index of @board using 'input_to_index'.
         if valid_move?(index)                     # The index is checked using 'valid_move', if satisfied the move takes place.
             move(index, current_player)           # If 'valid_move' is not satisfied, 'turn' is called again.
--- spec/titactoe_spec.rb
+++ spec/titactoe_spec.rb
@@ -1,4 +1,4 @@
-require '../spec/tictactoe'
+require '../lib/tictactoe'
 
 # Checkk all outcomes for game_over to be triggered
 
@@ -9,3 +9,18 @@ require '../spec/tictactoe'
 # Current player returns 'X' when the turn_count is even and 'O' when odd
 
 # Check outcomes for position taken when provided with data.
+
+describe TicTacToe do
+  describe '#valid_move?' do
+    it 'returns true for an empty spot' do
+      game = TicTacToe.new
+      expect(game.valid_move?(0)).to eq(true)
+    end
+
+    it 'returns false for a taken spot' do
+      game = TicTacToe.new
+      game.move(0, "X")
+      expect(game.valid_move?(0)).to eq(false)
+    end
+  end
+end
