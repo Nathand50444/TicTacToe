55232aa41f2815766c01bdd3cce19e04a0a8a071
NathanD <nathand50444@gmail.com>
2024-10-25 20:59:01 +0100

    Revert "Initial framework of 'game_over' testing"
    This reverts commit 0a7e494b501655b7947d72948d30138d2df91e57.

--- spec/titactoe_spec.rb
+++ spec/titactoe_spec.rb
@@ -1,6 +1,6 @@
-require '../lib/tictactoe'
+require '../spec/tictactoe'
 
-# Check all outcomes for game_over to be triggered
+# Checkk all outcomes for game_over to be triggered
 
 # 1. When there are no " " entries left then 'game_over' is true
 
@@ -9,25 +9,3 @@ require '../lib/tictactoe'
 # Current player returns 'X' when the turn_count is even and 'O' when odd
 
 # Check outcomes for position taken when provided with data.
-
-describe TicTacToe do
-
-  let(:game) { described_class.new }
-
-  context 'when the board shows the "X" "X" "X" sequence' do
-    let(:board) { ["X", "X", "X", "O", "O", " ", " ", " ", " "] }
-    it 'game_over returns true' do
-      expect(:board.game_over?).to eq true
-    end
-  end
-
-  describe '#game_over' do
-    context 'when the board shows the "X" "X" "X" sequence' do
-      it 'returns "Player X wins!"' do
-        @board = ["X", "X", "X", "O", "O", " ", " ", " ", " "] # Arrange
-        game_over
-        expect(game_over).to eq true # Assert
-      end
-    end
-  end
-end
